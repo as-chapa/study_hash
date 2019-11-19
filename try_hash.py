e98c4cdbff59b027f6f32c2f11939f454207e3a6
as-chapa <e.goromgorom@gmail.com>
2019-11-20 07:22:55 +0900

    [Mod] 処理内容を変えずにコードの見直し

--- try_hash.py
+++ try_hash.py
@@ -7,7 +7,21 @@ STR_COUNT = 10000
 
 org = sys.argv[1]
 
+def syoritime(func):
+    """
+    処理時間計測用のデコレータ
+    """
+    def wrapper(*args, **kargs):
+        start = time.time()
+        func(*args, **kargs)
+        syori_time = str(time.time() - start)
+        print(f'処理時間（ミリ秒）：{syori_time}')
+    return wrapper
+
 def randomsalt():
+    """
+    英字大文字小文字10桁をランダムに返す
+    """
     import string, random
     at = string.ascii_lowercase + string.ascii_uppercase
     return ''.join([random.choice(at) for i in range(10)])
@@ -20,9 +34,10 @@ def stretching(s):
         s = salthash(s)
     return s
 
-start = time.time()
-h_st = stretching(org)
-syori_time = time.time() - start
-print('処理時間（ミリ秒）：' + str(syori_time))
-print('元の値：' + org)
-print('ハッシュ値：' + h_st)
+@syoritime
+def main():
+    h_st = stretching(org)
+    print(f'元の値：{org}')
+    print(f'ハッシュ値：{h_st}')
+
+main()
